dab8665402216b2d8927ed3ff29b833e0bddd623
AndrejPer <43759884+AndrejPer@users.noreply.github.com>
2022-11-29 16:08:51 +0100

    updating relabeling.py

--- relabeling.py
+++ relabeling.py
@@ -1,9 +1,27 @@
+print("start")
 fp = open("mock.csv")
-alllines = fp.readlines()
-dict = dict()
-for line in alllines:
+all_lines = fp.readlines()
+dictionary = dict()
+ratios = []
+for line in all_lines:
     i = 0
-    print(line)
+    #print(line)
     pars_line = line.split("|||")
-    dict[0] = {"tag": pars_line[0], "left": pars_line[1]}
-    print(pars_line)
+    shorter = ""
+    longer = ""
+
+    if len(pars_line[1]) > len(pars_line[2]):
+        shorter = pars_line[2]
+        longer = pars_line[1]
+    else:
+        shorter = pars_line[1]
+        longer = pars_line[2]
+
+    dictionary[i] = {"tag": pars_line[0], "longer": longer, "shorter": shorter, "alignment": pars_line[4],
+                     "equivalence": pars_line[5].strip("\n"), "ratio": len(shorter) / len(longer)}
+    #ratios.append(dictionary[i]["ratio"])
+    #print(dictionary[i])
+    i = i + 1
+
+#print(ratios)
+print("end")
