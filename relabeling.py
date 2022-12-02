7fe464c7393801a3a3cf6e2cb0c8b47d21b513b9
AndrejPer <andrej.perkovic@icloud.com>
2022-12-02 09:17:00 +0100

    updating relabeling.py from readlines to readline becacause of the size

--- relabeling.py
+++ relabeling.py
@@ -1,12 +1,10 @@
 print("start")
 fp = open("ppdb-2.0-s-all.csv", "r")
 fnew = open("relabeled_ppdb_small.csv", "w")
-all_lines = fp.readlines()
-dictionary = dict()
-ratios = []
-for line in all_lines:
-    i = 0
-    #print(line)
+while True:
+    line = fp.readline()
+    if not line:
+        break
     pars_line = line.split(" ||| ")
     shorter = ""
     longer = ""
@@ -18,13 +16,10 @@ for line in all_lines:
         shorter = pars_line[1]
         longer = pars_line[2]
 
-    dictionary[i] = {"tag": pars_line[0], "longer": longer, "shorter": shorter, "alignment": pars_line[4],
-                     "equivalence": pars_line[5].strip("\n"), "ratio": len(shorter) / len(longer)}
-    print(longer)
+    print(pars_line[5])
     fnew.write(pars_line[0] + " ||| " + longer + " ||| " + shorter + " ||| " + pars_line[4] + " ||| " + str(len(shorter) / len(longer)) + " ||| " + pars_line[5])
-    #ratios.append(dictionary[i]["ratio"])
-    #print(dictionary[i])
-    i = i + 1
 
 #print(ratios)
 print("end")
+fp.close()
+fnew.close()
