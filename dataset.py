879868a500642f12f61506dd3d70f6ae9ac8ceb2
AndrejPer <andrej.perkovic@icloud.com>
2023-01-12 16:46:28 +0100

    cleaning sorting relabeled PPDB

--- dataset.py
+++ dataset.py
@@ -3,3 +3,6 @@ from datasets import load_dataset
 
 dataset = load_dataset("Helsinki-NLP/tatoeba_mt", "eng-srp_Cyrl")
 print("here")
+
+first10 = dataset['test'][slice(10, None, None)]
+print(first10)
