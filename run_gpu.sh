28ea6f154bb545288e0fea5573807aeac90a50b1
AndrejPer <andrej.perkovic@icloud.com>
2023-01-16 16:26:02 +0100

    correcting GPU bash script

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,9 +1,9 @@
 qsub -q gpu -l select=2:ngpus=2:gpu_mem=1gb:scratch_local=10gb -l walltime=1:00:00
 
 module add python/3.8.0-gcc
+source ../env/bin/activate
 
-python -m venv env
-
-source env/bin/activate
+pip install --upgrade pip setuptools wheel
+pip install datasets
 
 python dataset.py --num_sentences 5 --num_rules 5
