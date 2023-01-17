7f4a0571db295ed59303557230de4e9284f18606
AndrejPer <andrej.perkovic@icloud.com>
2023-01-17 12:50:51 +0100

    corrected sorting algorithm

--- run_gpu.sh
+++ run_gpu.sh
@@ -3,7 +3,4 @@ qsub -q gpu -l select=2:ngpus=2:gpu_mem=1gb:scratch_local=10gb -l walltime=1:00:
 module add python/3.8.0-gcc
 source ../env/bin/activate
 
-pip install --upgrade pip setuptools wheel
-pip install datasets
-
 python dataset.py --num_sentences 5 --num_rules 5
--- sorting.py
+++ sorting.py
@@ -1,34 +1,10 @@
-print("start")
-fp = open("relabeled_ppdb_small.csv", "r")
-fnew = open("sorted_ppdb_small.csv", "w")
-maxLine = ""
-maxVal = float('-inf')
-upperBound = float('inf')
-search = True
-while search:
-    # find the maximum ration and the line it appears at
-    fp = open("relabeled_ppdb_small.csv", "r") #open the file every time
-    while True:
-        line = fp.readline()
-        #print(line)
-        if not line:
-            break
+import pandas as pd
 
-        pars_line = line.split(" ||| ")
+# Read the input file into a Pandas dataframe
+df = pd.read_csv("relabeled_ppdb_small.csv", delimiter='\|\|\|', engine='python')
 
-        if maxVal < float(pars_line[4]) < upperBound:
-            maxVal = float(pars_line[4])
-            maxLine = line
+# Sort the dataframe by the value of the fifth field in ascending order
+df = df.sort_values(by=df.columns[4])
 
-    # checking if we should be done
-    if maxVal == upperBound:
-        print("we done here")
-        break
-    upperBound = maxVal
-    maxVal = float('-inf')
-    # we found the next biggest line, we now put it
-    fnew.write(maxLine)
-    print(f"added {maxLine}")
-
-fp.close()
-fnew.close()
+# Write the sorted dataframe to the output file one line at a time
+df.to_csv("sorted_ppdb_small.csv", sep='|', index=False)
