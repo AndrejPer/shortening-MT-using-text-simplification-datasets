1bf85183fdc81278a8cd6149405ef4646fc1f449
AndrejPer <andrej.perkovic@icloud.com>
2023-01-17 12:50:51 +0100

    corrected sorting algorithm

--- dataset.py
+++ dataset.py
@@ -2,18 +2,7 @@ from datasets import load_dataset
 import argparse
 import re
 
-
-
-
-dataset = load_dataset("Helsinki-NLP/tatoeba_mt", "eng-srp_Cyrl")
-print("here")
-
-
-
-#print(len(dataset['test']['sourceString']))
-sourceStrings = dataset['test']['sourceString'][:10]
-print(sourceStrings)
-
+#PARSING
 parser = argparse.ArgumentParser()
 parser.add_argument("--num_sentences", type = int, help="Number of first Y source sentences to apply rules to")
 parser.add_argument("--num_rules", type=int, help="Number of first X rules from PPDB instance to apply")
@@ -21,7 +10,32 @@ args = parser.parse_args()
 
 print("Modifying", args.num_sentences, "sentences using", args.num_rules, "paraphrasing rules.")
 
+
+#GETTING SOURCE SENTENCES
+#dataset = load_dataset("Helsinki-NLP/tatoeba_mt", "eng-srp_Cyrl")
+#sourceStrings = dataset['test']['sourceString'][:args.num_sentences]
+sourceStrings = ['A cat is not a person!', 'Actions speak louder than words.', 'Add more water to it.', "A dream... I was trying to explain to St. Peter, and was doing it in the German tongue, because I didn't want to be too explicit.", 'After long reflection, I decided to take things as they come.', 'After silence, that which comes nearest to expressing the inexpressible is music.', 'After three years of work by volunteers, the system has been fully repaired.', 'After you.', 'A gluten-free diet is the most effective treatment for coeliac disease.', 'A golden key opens all doors.']
+print(sourceStrings)
+
+#LOADING RULES
+frules = open("sorted_ppdb_small.csv", "r")
+rules = []
+for i in range(args.num_rules):
+    line = frules.readline()
+    #in case of illegal argument
+    if not line:
+        break
+    pars_line = line.split(" ||| ")
+    rules.append({"tag": pars_line[0], "longer": pars_line[1], "shorter": pars_line[2], "score": pars_line[3], "ratio": pars_line[4], "equivalence": pars_line[5]}) #dictionary of rules!
+
+
+#APPLYING RULES
 for sentence in sourceStrings:
-    words = words = re.findall(r'\b\w+\b', sentence)
-    for word in words:
-        print(word)
+    for rule in rules:
+        if rule["shorter"] == "$":
+            pattern = re.compile(r"\$")
+        else:
+            pattern = re.compile(rule["shorter"])
+        print(re.findall(pattern, sentence))
+        sentence = re.sub(pattern, rule["longer"], sentence)
+        print(sentence)
