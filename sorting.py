aaf860a1716c2b94fc4998998aa215abf05c6272
AndrejPer <andrej.perkovic@icloud.com>
2023-01-25 21:36:39 +0100

    applying rules through dataframe object

--- sorting.py
+++ sorting.py
@@ -1,9 +1,14 @@
 import pandas as pd
 
-df = pd.read_csv("relabeled_ppdb_small.csv", delimiter='\|\|\|', engine='python')
+column_names = ['Tag', 'Longer', 'Shorter', 'Score', 'Ratio', 'Equivalence']
+df = pd.read_csv("relabeled_ppdb_small.csv", delimiter=' \|\|\| ', engine='python', names=column_names)
 df = df.drop_duplicates()
+print(df)
 
 # Sort the dataframe by the value of the fifth field in ascending order
 df = df.sort_values(by=df.columns[4])
+print(df)
+df = df[df['Equivalence'] == 'Equivalence']
+print(df)
 
 df.to_csv("sorted_ppdb_small.csv", sep='|', index=False)
