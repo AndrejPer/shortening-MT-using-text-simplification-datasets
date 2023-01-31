9c9fc438641c3e3d0d6ace998ecc49c280fa1630
AndrejPer <andrej.perkovic@icloud.com>
2023-01-31 19:55:36 +0100

    adapting dataset.py to the new dataset OPUS-100

--- dataset.py
+++ dataset.py
@@ -5,18 +5,19 @@ import pandas as pd
 
 #PARSING
 parser = argparse.ArgumentParser()
+# Set the parameters
 parser.add_argument("--num_sentences", type = int, default=50, help="Number of first Y source sentences to apply rules to")
 parser.add_argument("--num_rules", type=int, default=100, help="Number of first X rules from PPDB instance to apply")
 args = parser.parse_args()
-
-print(f"Modifying {args.num_sentences} sentences using {args.num_rules}, paraphrasing rules.")
+print(f"Modifying {args.num_sentences} sentences using {args.num_rules} paraphrasing rules.")
 
 
 #GETTING SOURCE SENTENCES
-dataset = load_dataset("Helsinki-NLP/tatoeba_mt", "eng-srp_Cyrl")
-source_sentences = dataset['validation']['sourceString'][:args.num_sentences]
-#source_strings = ['A cat is not a person!', 'Actions speak louder than words.', 'Add more water to it.', "A dream... I was trying to explain to St. Peter, and was doing it in the German tongue, because I didn't want to be too explicit.", 'After long reflection, I decided to take things as they come.', 'After silence, that which comes nearest to expressing the inexpressible is music.', 'After three years of work by volunteers, the system has been fully repaired.', 'After you.', 'A gluten-free diet is the most effective treatment for coeliac disease.', 'A golden key opens all doors.']
-
+with open("opus-100/opus.en-sr-train.en") as f:
+    lines = f.readlines()
+    fields = [line.strip().split('\n') for line in lines]
+    source_sentences = pd.DataFrame(fields)
+print(source_sentences[0][10])
 
 
 #APPLYING RULES
@@ -25,21 +26,18 @@ rules = pd.read_csv("sorted_ppdb_small.csv", delimiter='\|', engine='python')
 # Counter for the number of changes made
 counter = 0
 
-for sentence in source_sentences:
+for i, sentence in enumerate(source_sentences.to_numpy()):
+    print(sentence[0])
     for index in range(args.num_rules):
-        #print(rules["Shorter"][index])
         # Making the pattern with whitespace, so it doesn't change 'Facebook' to 'Facebookay'
         pattern = re.compile(re.escape(" " + rules["Shorter"][index].strip() + " "))
 
-
-        changes = len(re.findall(pattern, sentence))
+        changes = len(re.findall(pattern, sentence[0]))
         if changes != 0:
             print(changes)
-            counter = counter + len(re.findall(pattern, sentence))
-            print(f"Before: {sentence}")
-            sentence = re.sub(pattern, rules["Longer"][index], sentence)
-            print(f"After: {sentence}")
-
-
+            counter = counter + len(re.findall(pattern, sentence[0]))
+            print(f"Before: {sentence[0]}")
+            sentence[0] = re.sub(pattern, rules["Longer"][index], sentence[0])
+            print(f"After: {sentence[0]}")
 
 print(counter)
