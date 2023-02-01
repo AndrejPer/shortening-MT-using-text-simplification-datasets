df761e9892c4e01d716bc47a73b8baf7a91caa87
AndrejPer <andrej.perkovic@icloud.com>
2023-02-01 10:13:29 +0100

    removing debugging

--- dataset.py
+++ dataset.py
@@ -27,7 +27,7 @@ rules = pd.read_csv("sorted_ppdb_small.csv", delimiter='\|', engine='python')
 counter = 0
 
 for i, sentence in enumerate(source_sentences.to_numpy()):
-    print(sentence[0])
+    #print(sentence[0])
     for index in range(args.num_rules):
         # Making the pattern with whitespace, so it doesn't change 'Facebook' to 'Facebookay'
         pattern = re.compile(re.escape(" " + rules["Shorter"][index].strip() + " "))
@@ -36,8 +36,8 @@ for i, sentence in enumerate(source_sentences.to_numpy()):
         if changes != 0:
             print(changes)
             counter = counter + len(re.findall(pattern, sentence[0]))
-            print(f"Before: {sentence[0]}")
+            #print(f"Before: {sentence[0]}")
             sentence[0] = re.sub(pattern, rules["Longer"][index], sentence[0])
-            print(f"After: {sentence[0]}")
+            #print(f"After: {sentence[0]}")
 
 print(counter)
