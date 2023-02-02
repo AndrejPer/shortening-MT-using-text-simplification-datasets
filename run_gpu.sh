e006ecee8ba428c07fa52d5bf7e9488f3405f9eb
AndrejPer <andrej.perkovic@icloud.com>
2023-02-02 18:37:57 +0100

    modifying bash script to run modification on complete corpus

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,6 +1,6 @@
-qsub -q gpu -l select=2:ngpus=2:gpu_mem=1gb:scratch_local=10gb -l walltime=1:00:00
+qsub -q gpu -l select=2:ngpus=2:gpu_mem=1gb:scratch_local=10gb -l walltime=24:00:00
 
 module add python/3.8.0-gcc
 source ../env/bin/activate
 
-python dataset.py --num_sentences 5 --num_rules 5
+python dataset.py
