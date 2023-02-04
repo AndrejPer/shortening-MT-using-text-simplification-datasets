4a01b66e71ba2f1c2ce10e2cfb1373f660b46e8c
AndrejPer <andrej.perkovic@icloud.com>
2023-02-04 15:19:52 +0100

    turning csvs into dataset

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-dataset = load_dataset("csv", data_files={"train": ["opus-100/opus_7007_1000000.en-sr-train.en", "opus-100/opus.en-sr-train.sr"], "test": ["opus-100/opus.en-sr-test.en", "opus-100/opus.en-sr-test.sr"], "dev": ["opus-100/opus.en-sr-dev.en", "opus-100/opus.en-sr-dev.sr"]})
+dataset = load_dataset("csv", data_files={"dev": ["opus-100/opus.en-sr-dev.en", "opus-100/opus.en-sr-dev.sr"], "train": ["opus-100/opus_7007_1000000.en-sr-train.en", "opus-100/opus.en-sr-train.sr"], "test": ["opus-100/opus.en-sr-test.en", "opus-100/opus.en-sr-test.sr"]})
 
 print(dataset)
 
