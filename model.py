a26f4f3c5a44de83d53f74b06d4302a8583adb7b
AndrejPer <andrej.perkovic@icloud.com>
2023-02-04 14:29:50 +0100

    turning csvs into dataset

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from datasets import load_dataset
+dataset = load_dataset("csv", data_files={"train": ["opus-100/opus.en-sr-train.en", "opus-100/opus.en-sr-train.sr"], "test": ["opus-100/opus.en-sr-test.en", "opus-100/opus.en-sr-test.sr"], "dev": ["opus-100/opus.en-sr-dev.en", "opus-100/opus.en-sr-dev.sr"]})
 
-tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-tc-base-en-sh")
+print(dataset)
 
-model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-tc-base-en-sh")
+
