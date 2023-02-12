d5aa98530b035a66c1593e8bb1c58af8bae1ee6b
AndrejPer <andrej.perkovic@icloud.com>
2023-02-12 23:48:15 +0100

    training script

--- run_gpu.sh
+++ run_gpu.sh
@@ -1,6 +1,8 @@
-qsub -q gpu -l select=2:ngpus=2:gpu_mem=1gb:scratch_local=10gb -l walltime=24:00:00
+qsub -I -l select=1:mem=16gb:scratch_local=10gb:ngpus=1:gpu_cap=cuda60:cuda_version=11.0 -q gpu -l walltime=4:00:00
+singularity shell --nv /cvmfs/singularity.metacentrum.cz/NGC/TensorFlow\:21.03-tf2-py3.SIF
 
 module add python/3.8.0-gcc
 source ../env/bin/activate
+pip install sacrebleu
 
 python dataset.py
