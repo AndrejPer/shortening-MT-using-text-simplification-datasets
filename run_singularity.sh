b84237812eca976fc95902f0cf8fc12ea3cc042f
AndrejPer <andrej.perkovic@icloud.com>
2023-02-15 23:10:16 +0100

    correcting scripts for the server

--- run_singularity.sh
+++ run_singularity.sh
@@ -3,5 +3,7 @@
 #PBS -q gpu
 #PBS -l select=1:ncpus=1:mem=16gb:scratch_local=10gb:ngpus=16:gpu_cap=cuda60
 #PBS -l walltime=10:00:00
-#PBS -m ae
-singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch\:20.09-py3.SIF /storage/praha1/home/andrejp/MT/shortening-MT-using-text-simplification-datasets/run_gpu.sh
+#PBS -m abe
+#PBS -o z_output
+#PBS -e z_error
+singularity run --nv /cvmfs/singularity.metacentrum.cz/NGC/PyTorch\:20.09-py3.SIF bash /storage/praha1/home/andrejp/MT/shortening-MT-using-text-simplification-datasets/run_gpu.sh
