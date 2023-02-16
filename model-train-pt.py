baaef0b4778651a6c5b0a4e5a6050513ebdedc05
AndrejPer <andrej.perkovic@icloud.com>
2023-02-16 18:56:17 +0100

    correcting paths

--- model-train-pt.py
+++ model-train-pt.py
@@ -2,10 +2,10 @@ from datasets import load_dataset, DatasetDict, Dataset
 import pandas as pd
 
 
-a_file = open("opus-100/opus_7007_1000000.en-sr-train.en")
+a_file = open("opus_7007_1000000.en-sr-train.en")
 file_contents = a_file.read()
 en_train_split = file_contents.splitlines()
-a_file = open("opus-100/opus.en-sr-train.sr")
+a_file = open("opus.en-sr-train.sr")
 file_contents = a_file.read()
 sr_train_split = file_contents.splitlines()
 
@@ -13,10 +13,10 @@ train = {'translation': [{"en": eng_text, "sr": srb_text} for eng_text, srb_text
 train = Dataset.from_dict(train)
 
 
-a_file = open("opus-100/opus.en-sr-test.sr")
+a_file = open("opus.en-sr-test.sr")
 file_contents = a_file.read()
 sr_test_split = file_contents.splitlines()
-a_file = open("opus-100/opus.en-sr-test.en")
+a_file = open("opus.en-sr-test.en")
 file_contents = a_file.read()
 en_test_split = file_contents.splitlines()
 
@@ -24,10 +24,10 @@ test = {'translation': [{"en": eng_text, "sr": srb_text} for eng_text, srb_text 
 test = Dataset.from_dict(test)
 
 
-a_file = open("opus-100/opus.en-sr-dev.sr")
+a_file = open("opus.en-sr-dev.sr")
 file_contents = a_file.read()
 sr_dev_split = file_contents.splitlines()
-a_file = open("opus-100/opus.en-sr-dev.en")
+a_file = open("opus.en-sr-dev.en")
 file_contents = a_file.read()
 en_dev_split = file_contents.splitlines()
 
