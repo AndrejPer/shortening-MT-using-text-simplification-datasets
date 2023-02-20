4487628e001fd1683779ef3e98985b8bfa68bb5a
AndrejPer <andrej.perkovic@icloud.com>
2023-02-20 15:55:51 +0100

    scripts for fine-tuned translation of test set

--- run.sh
+++ run.sh
@@ -3,11 +3,10 @@
 #PBS -l select=1:ncpus=2:mem=4gb:scratch_local=10gb
 #PBS -l walltime=10:00:00
 #PBS -m abe
-#PBS -o z_hardware
-#PBS -e z_time
 
 # define a DATADIR variable: directory where the input files are taken from and where output will be copied to
 DATADIR=/storage/praha1/home/andrejp/MT/shortening-MT-using-text-simplification-datasets
+PYTHONPROG=fine-tuned_translation.py
 
 echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
 
@@ -20,13 +19,8 @@ test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 
 # copy input files to scratch directory
 # if the copy operation fails, issue error message and exit
-cp $DATADIR/opus-100/opus_7007_1000000.en-sr-train.en  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-train.sr  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-test.en  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-test.sr  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-dev.en  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-dev.sr  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/model-train-pt.py  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
+cp -r $DATADIR/Helsinki-NLP $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
+cp $DATADIR/$PYTHONPROG  $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
 
 # installing packages
 # pip install datasets transformers sentencepiece sacrebleu
@@ -35,12 +29,13 @@ cp $DATADIR/model-train-pt.py  $SCRATCHDIR || { echo >&2 "Error while copying in
 # move into scratch directory
 cd $SCRATCHDIR
 
-# run fine-tuning script
+# run Python script
 source $DATADIR/../env/bin/activate
-python $DATADIR/raw_translation.py || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
+python $DATADIR/$PYTHONPROG || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
 
 # move the output to user's DATADIR or exit in case of failure
-cp -r ./* $DATADIR/results_4 || { echo >&2 "Result file(s) copying failed (with a code $?) !!"; exit 4; }
+mkdir $DATADIR/results_$PBS_JOBID
+cp -r ./* $DATADIR/results_$PBS_JOBID || { echo >&2 "Result file(s) copying failed (with a code $?) !!"; exit 4; }
 
 # clean the SCRATCH directory
 clean_scratch
