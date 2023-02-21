9d8c6143e983d342f8e4baeabe2bd5b70f222466
AndrejPer <andrej.perkovic@icloud.com>
2023-02-21 12:15:39 +0100

    debugging translation

--- run.sh
+++ run.sh
@@ -29,6 +29,7 @@ cd $SCRATCHDIR
 
 # run Python script
 source $DATADIR/../env/bin/activate
+echo $DATADIR/$PYTHONPROG
 python $DATADIR/$PYTHONPROG || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
 
 # move the output to user's DATADIR or exit in case of failure
