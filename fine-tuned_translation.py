75a8142af50d9e02db1d12e90922fb76a80072e8
AndrejPer <andrej.perkovic@icloud.com>
2023-02-21 11:04:00 +0100

    debugging translation

--- fine-tuned_translation.py
+++ fine-tuned_translation.py
@@ -9,13 +9,16 @@ en_test_split = file_contents.splitlines()
 
 test = {'translation': [{"en": eng_text, "sr": srb_text} for eng_text, srb_text in zip(en_test_split, sr_test_split)]}
 test_sentences = [sentence for sentence in en_test_split]
+print("test set formed")
 
 # loading the model and translating test set again
 model_checkpoint = "./Helsinki-NLP/finetuned-opus-mt-tc-base-en-sh"
 tokenizer = MarianTokenizer.from_pretrained(model_checkpoint)
 model = MarianMTModel.from_pretrained(model_checkpoint)
+print("loaded model")
 padded_test_set = [">>>srp_Latn<<< " + sentence for sentence in test_sentences]
 translated = model.generate(**tokenizer(test_sentences, return_tensors="pt", padding=True))
 trans_file = open("fine-tuned_translation.txt", "w")
 for t in translated:
+    print(tokenizer.decode(t, skip_special_tokens=True) + "\n")
     trans_file.write(tokenizer.decode(t, skip_special_tokens=True) + "\n")
