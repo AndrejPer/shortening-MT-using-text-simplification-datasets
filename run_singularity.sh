0955339409850f44b55e85e7a549c2462818016d
AndrejPer <andrej.perkovic@icloud.com>
2023-05-14 14:12:20 +0200

    scripts for evaluating pre-PPDB model

--- run_singularity.sh
+++ run_singularity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N trans_short
+#PBS -N eval_normal
 #PBS -q gpu
-#PBS -l select=1:ncpus=1:mem=128gb:scratch_local=128gb:ngpus=1
+#PBS -l select=1:ncpus=1:mem=64gb:scratch_local=64gb:ngpus=1
 #PBS -l walltime=5:00:00
 #PBS -m abe
 
