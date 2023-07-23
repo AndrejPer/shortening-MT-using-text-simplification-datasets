3acdd87d9e02b993795da991c997890ad4b5d192
AndrejPer <andrej.perkovic@icloud.com>
2023-07-23 21:10:56 +0200

    scripts for applying XXL size lexical rules, again

--- run_singularity.sh
+++ run_singularity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#PBS -N trans_raw
-#PBS -l select=1:ncpus=1:mem=128gb:scratch_local=128gb
-#PBS -l walltime=8:00:00
+#PBS -N app_XXL
+#PBS -l select=1:ncpus=1:mem=64gb:scratch_local=64gb
+#PBS -l walltime=5:00:00
 #PBS -m abe
 
 export PYTHONUSERBASE=/storage/praha1/home/andrejp/PUB-PyTorch21.11
