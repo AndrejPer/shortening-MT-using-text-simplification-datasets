6c1d99bcd5b0d3b9d29c7ca933ac0184f7fdd3a4
AndrejPer <andrej.perkovic@icloud.com>
2023-07-26 08:49:52 +0200

    scripts for translating with M tagged srp_Latn

--- util/translation.py
+++ util/translation.py
@@ -6,13 +6,13 @@ en_test_split = fp.read().splitlines()
 test_sentences = [sentence for sentence in en_test_split]
 
 # loading the model and translating test set again
-model_checkpoint = "./Helsinki-NLP/shortS-opus-mt-tc-base-en-sh"
+model_checkpoint = "./Helsinki-NLP/shortM-opus-mt-tc-base-en-sh"
 tokenizer = MarianTokenizer.from_pretrained(model_checkpoint)
 model = MarianMTModel.from_pretrained(model_checkpoint)
 padded_test_set = [">>srp_Latn<< " + sentence for sentence in test_sentences]
 translated = model.generate(**tokenizer(test_sentences,
                                         return_tensors="pt", padding=True))
-trans_file = open("short_S_translation.txt", "w")
+trans_file = open("short_M_translation.txt", "w")
 for t in translated:
     trans_file.write(tokenizer.decode(t, skip_special_tokens=True) + "\n")
 
