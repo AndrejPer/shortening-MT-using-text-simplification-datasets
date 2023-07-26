bb0552f43116c9d5cc4cff25e2eb94934f22bae0
AndrejPer <andrej.perkovic@icloud.com>
2023-07-26 11:26:24 +0200

    scripts for translating with M tagged srp_Latn

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # define a DATADIR variable: directory where the input files are taken from and where output will be copied to
 DATADIR=/storage/praha1/home/andrejp/MT/shortening-MT-using-text-simplification-datasets
-PYTHONPROG=model-train-pt.py
-PROGDIR=./
+PYTHONPROG=translation.py
+PROGDIR=util
 echo "$PBS_JOBID is running on node `hostname -f` in a scratch directory $SCRATCHDIR" >> $DATADIR/jobs_info.txt
 # load Python
 module add python/3.8.0-gcc
@@ -13,12 +13,10 @@ test -n "$SCRATCHDIR" || { echo >&2 "Variable SCRATCHDIR is not set!"; exit 1; }
 # copy input files to scratch directory
 # if the copy operation fails, issue error message and exit
 cp $DATADIR/$PROGDIR/$PYTHONPROG $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus_34654_1000000_xxl.en-sr-train.en $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/corrected.opus.en-sr-train.sr $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
 cp $DATADIR/opus-100/opus.en-sr-test.en $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
 cp $DATADIR/opus-100/corrected.opus.en-sr-test.sr $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-dev.en $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
-cp $DATADIR/opus-100/opus.en-sr-dev.sr $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
+cp -r $DATADIR/results_just_train_M/Helsinki-NLP $SCRATCHDIR || { echo >&2 "Error while copying input file(s)!"; exit 2; }
+
 
 # move into scratch directory
 cd $SCRATCHDIR
@@ -28,7 +26,7 @@ source $DATADIR/../env/bin/activate
 python $PYTHONPROG || { echo >&2 "Calculation ended up erroneously (with a code $?) !!"; exit 3; }
 
 # move the output to user's DATADIR or exit in case of failure
-NEWDIR=results_just_train_XXL
+NEWDIR=results_just_trans_M
 [[ -d $DATADIR/$NEWDIR ]] || mkdir $DATADIR/$NEWDIR
 cp -r ./* $DATADIR/$NEWDIR || { echo >&2 "Result file(s) copying failed (with a code $?) !!"; exit 4; }
 
