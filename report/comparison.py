0c8100cbce4a55bff794960499b07177d21e9c71
AndrejPer <andrej.perkovic@icloud.com>
2023-08-10 21:42:46 +0200

    utility script

--- report/comparison.py
+++ report/comparison.py
@@ -1,12 +1,10 @@
-s = "Nice. To. Meet. You."
-print(len(list(filter(str.isalpha, s))))
 
-raw = open("../translations/normal_translation.txt", "r")
+raw = open("../opus-100/opus.en-sr-train.en", "r")
 raw_lines = raw.readlines()
 raw.close()
 raw_trans = "".join(raw_lines)
 
-fine = open("../translations/fine-tuned_translation.txt", "r")
+fine = open("../sorted_ppdb", "r")
 fine_lines = fine.readlines()
 fine.close()
 fine_trans = "".join(fine_lines)
